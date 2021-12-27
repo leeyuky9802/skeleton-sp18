ab58b9d5f05ba69286c9730282ee6a475da4d0d4
Xiao Yang <leeyuky9802@gmail.com>
2021-12-28 06:24:17 +0800

    what's wrong with intellij

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,89 @@
-package PACKAGE_NAME;public class ArrayDeque {
+public class ArrayDeque<Item> {
+    Item[] items;
+    int length;
+    int size;
+    int nextLast;
+    int nextFirst;
+    public ArrayDeque(){
+        items =(Item[]) new Object[8];
+        size=0;
+        nextFirst=3;
+        nextLast=4;
+        length=8;
+    }
+    public void addFirst(Item item){
+        items[nextFirst]=item;
+        nextFirst--;
+        nextFirst+=length;
+        nextFirst%=length;
+        size++;
+        check();
+
+    }
+    public void addLast(Item item){
+        items[nextLast]=item;
+        nextLast++;
+        nextLast+=length;
+        nextLast%=length;
+        size++;
+        check();
+
+    }
+    public boolean isEmpty(){ if(size==0) return true;else return false;}
+    public int size(){return size;}
+    public void printDeque(){
+        int p=nextFirst;
+        p++;
+        p%=length;
+        for(int i=0;i<size;i++){
+            System.out.print(items[p]+" ");
+            p++;
+            p%=length;
+
+        }
+    }
+    public Item removeFirst(){
+        Item temp;
+        nextFirst++;
+        nextFirst+=length;
+        nextFirst%=length;
+        size--;
+        temp=items[nextFirst];
+        check();
+        return temp;
+    }
+    public Item removeLast(){
+        Item temp;
+        nextLast--;
+        nextLast+=length;
+        nextLast%=length;
+        size--;
+        temp=items[nextLast];
+        check();
+        return temp;
+    }
+    public Item get(int Index){
+        int i=nextFirst+Index+1;
+        i%=length;
+        return items[i];
+    }
+    public void check(){
+        if(nextLast==nextFirst) rearrange(length*2);
+        if(length>=16&&4*size<length) rearrange(length/2);
+    }
+    public void rearrange(int newSize){
+        int l=length;
+        length=newSize;
+        Item[] temp = (Item[]) new Object[newSize];
+        nextFirst++;
+        nextFirst%=l;
+        for(int i=0;i<size;i++){
+            temp[i]=items[nextFirst];
+            nextFirst++;
+            nextFirst%=l;
+        }
+        nextFirst=length-1;
+        nextLast=size;
+        items=temp;
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class LinkedListDeque {
+public class LinkedListDeque<Item> {
+    private class Node{
+        Item item;
+        Node next;
+        Node previous;
+    }
+    private Node sentinel;
+    private int size;
+
+    public LinkedListDeque(){
+        sentinel = new Node();
+        sentinel.next=sentinel;
+        sentinel.previous=sentinel;
+        size=0;
+    }
+
+    public void addFirst(Item item){
+        Node temp = new Node();
+        temp.item=item;
+        temp.next=sentinel.next;
+        temp.previous=sentinel;
+        sentinel.next.previous=temp;
+        sentinel.next=temp;
+        size++;
+    }
+    public void addLast(Item item){
+        Node temp = new Node();
+        temp.item=item;
+        temp.next=sentinel;
+        temp.previous=sentinel.previous;
+        sentinel.previous.next=temp;
+        sentinel.previous=temp;
+        size++;
+    }
+    public boolean isEmpty(){
+        if(size==0) return true;
+        else return false;
+    }
+    public int size() { return size;}
+    public void printDeque(){
+        Node p = sentinel;
+        if(p.next==sentinel) return;
+        p=p.next;
+        while(p!=sentinel){
+            System.out.print(p.item+" ");
+            p=p.next;
+        }
+    }
+    public Item removeLast(){
+        Node ret=sentinel.previous;
+        sentinel.previous=sentinel.previous.previous;
+        sentinel.previous.next=sentinel;
+        size--;
+        return ret.item;
+    }
+    public Item removeFirst(){
+        Node ret=sentinel.next;
+        sentinel.next=sentinel.next.next;
+        sentinel.next.previous=sentinel;
+        size--;
+        return ret.item;
+    }
+
+    public Item get(int Index){
+        if(Index>=size) return null;
+        Node p=sentinel;
+        for(int i=0;i<=Index;i++){
+            p=p.next;
+        }
+        return p.item;
+    }
+
+    public Item getRecursive(int Index){
+        return getRecursiveHelper(Index,sentinel);
+    }
+
+    public Item getRecursiveHelper(int Index,Node node){
+        if(Index==-1) return node.item;
+        else{
+            return getRecursiveHelper(Index-1,node.next);
+        }
+    }
 }
