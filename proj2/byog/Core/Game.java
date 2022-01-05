f47ad70e7873ab532d6f812d6dfe75031f3eb90c
Xiao Yang <leeyuky9802@gmail.com>
2022-01-05 13:00:32 +0800

    random world generater

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -1,7 +1,13 @@
 package byog.Core;
 
-import byog.TileEngine.TERenderer;
-import byog.TileEngine.TETile;
+import byog.TileEngine.*;
+import edu.princeton.cs.introcs.In;
+
+import java.time.chrono.ThaiBuddhistEra;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ThreadFactory;
 
 public class Game {
     TERenderer ter = new TERenderer();
@@ -28,11 +34,156 @@ public class Game {
      * @return the 2D TETile[][] representing the state of the world
      */
     public TETile[][] playWithInputString(String input) {
-        // TODO: Fill out this method to run the game using the input passed in,
-        // and return a 2D tile representation of the world that would have been
-        // drawn if the same inputs had been given to playWithKeyboard().
+        Random random = new Random(seedGen(input));
+        int width = random.nextInt(60);
+        width+=50;
+        int height = random.nextInt(30);
+        height +=25;
+        int roomNum= random.nextInt(4);
+        roomNum+=10;
+        int[] xs = new int[roomNum];
+        int[] ys = new int[roomNum];
+        int[][] map = new int[300][300];
+        for(int[] i:map){
+            for(int j:i){
+                j=0;
+            }
+        }
 
-        TETile[][] finalWorldFrame = null;
-        return finalWorldFrame;
+
+
+        int count =0;
+        while(count!=roomNum){
+            int dx= random.nextInt(width-1);
+            int dy= random.nextInt(height-1);
+                xs[count]=dx;
+                ys[count]=dy;
+                count++;
+        }
+
+
+
+
+        int[] finalRoom = new int[roomNum];
+        int countR=1;
+        //paint and numerate the rooms
+        for(int i=0;i<roomNum;i++){
+            if(map[xs[i]][ys[i]]==0){
+                for(int kk=0;kk<20;kk++){
+                    int w=random.nextInt(width/2);
+                    int h=random.nextInt(height/2);
+                    w+=2;
+                    h+=2;
+                    if(checkRoom(xs[i],ys[i],w,h,map,countR)){
+                        finalRoom[countR-1] = i;
+                        countR++;
+                        break;
+                    }
+                }
+            }
+        }
+        countR--;
+
+        Set<Integer> connected = new HashSet<>();
+        Set<Integer> inconnected = new HashSet<>();
+        connected.add(finalRoom[0]);
+        for(int i=1;i<countR;i++) inconnected.add(finalRoom[i]);
+        while(!inconnected.isEmpty()){
+            int conee1 = randomSelect(connected,random);
+            int conee2 = randomSelect(inconnected,random);
+            inconnected.remove(conee2);
+            connected.add(conee2);
+            for(int i=Math.min(xs[conee1],xs[conee2]);i<=Math.max(xs[conee1],xs[conee2]);i++){
+                map[i][ys[conee1]] = countR;
+            }
+            for(int i=Math.min(ys[conee1],ys[conee2]);i<=Math.max(ys[conee1],ys[conee2]);i++){
+                map[xs[conee2]][i] = countR;
+            }
+        }
+
+        for(int i=1;i<width;i++){
+            for(int j=1;j<width;j++){
+                if(map[i][j]==0){
+                    if(map[i-1][j]>0||map[i+1][j]>0||map[i][j-1]>0||map[i][j+1]>0) map[i][j] =-1;
+                }
+            }
+        }
+
+        for(int i=1;i<width;i++){
+            if(map[i][0]==0){
+                if(map[i-1][0]>0||map[i+1][0]>0||map[i][1]>0) map[i][0] =-1;
+            }
+        }
+        for(int j=1;j<height;j++){
+            if(map[0][j]==0){
+                if(map[0][j-1]>0||map[0][j+1]>0||map[1][j]>0) map[0][j] =-1;
+            }
+        }
+
+        if(map[1][0]>0||map[0][1]>0) map[0][0] =-1;
+
+        TETile[][] world = mapToWorld(map,width,height);
+
+/*        TERenderer ter = new TERenderer();
+        ter.initialize(width,height);
+        ter.renderFrame(world);*/
+
+
+        return world;
+    }
+
+    private int randomSelect(Set<Integer> myhashSet,Random random){
+        int size = myhashSet.size();
+        int item = random.nextInt(size);
+        int i = 0;
+        for(int obj : myhashSet)
+        {
+            if (i == item) return obj;
+        }
+        return 0;
+    }
+
+    private long seedGen(String s){
+
+        long rt = 0;
+        char[] ch= s.toCharArray();
+        if(ch.length<3) {
+            for (int i = 0; i < ch.length; i++) {
+                rt += ch[i];
+            }
+        }else{
+            for (int i = 0; i < ch.length; i++) {
+                rt += ch[i];
+            }
+        }
+        return rt;
+    }
+
+
+
+    private boolean checkRoom(int x,int y,int w,int h,int[][] map,int count){
+        for(int i=0;i<w;i++){
+            for(int j=0;j<h;j++){
+                if(map[x+i][y+j]!=0) return false;
+            }
+        }
+        for(int i=0;i<w;i++){
+            for(int j=0;j<h;j++){
+                map[x+i][y+j]=count;
+            }
+        }
+        return true;
+    }
+
+    private TETile[][] mapToWorld(int[][] map, int width, int height){
+        TETile[][] rt= new TETile[width][height];
+        for(int i=0;i<width;i++){
+            for(int j=0;j<height;j++){
+                if(map[i][j]==0){
+                    rt[i][j] = Tileset.NOTHING;
+                }else if(map[i][j]>0) rt[i][j] = Tileset.FLOOR;else rt[i][j]=Tileset.WALL;
+            }
+        }
+        return rt;
     }
 }
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,43 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    public static void addHexagon(TETile[][] tiles,int size,int x,int y){
+
+        int dSize=2;
+        int bound =y-size;
+        //check(tiles.length,tiles[0].length,size,x,y);
+        for(;y>bound;y--){
+            for(int j=x;j<x+dSize;j++){
+                tiles[j][y]= Tileset.WALL;
+            }
+            dSize+=2;
+            x--;
+        }
+        bound-=size;
+        dSize-=2;
+        x+=1;
+        for(;y>bound;y--){
+            for(int j=x;j<x+dSize;j++){
+                tiles[j][y]= Tileset.WALL;
+            }
+            dSize-=2;
+            x++;
+        }
+    }
+
+    public static void main(String args[]){
+        TERenderer ter= new TERenderer();
+        TETile[][] tiles = new TETile[10][10];
+        ter.initialize(10,10);
+        for (int x = 0; x < 10; x += 1) {
+            for (int y = 0; y <10; y += 1) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+
+
+        ter.renderFrame(tiles);
+    }
+
+    
 }
